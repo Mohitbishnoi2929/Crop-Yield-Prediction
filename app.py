0855ddfd77b1990c586efc474bce5007fc491a0f
Mohitbishnoi2929 <bishnoimohit1999@gmail.com>
2026-02-27 21:45:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,30 +6,16 @@ st.title("🌾 Crop Yield Analytics Platform")
 
 st.markdown("## 🚀 Navigate Through the Platform")
 
-st.markdown("""
-<div style="font-size:18px; line-height:2.2;">
+col1, col2, col3 = st.columns(3)
 
-<a href="/Predictive_page" 
-title="Go to crop yield prediction page where you can input climate and soil data to predict yield"
-style="text-decoration:none; color:black; font-weight:bold;">
-🔮 Prediction
-</a>
+with col1:
+    if st.button("🔮 Prediction"):
+        st.switch_page("pages/Predictive_page.py")
 
-<br><br>
+with col2:
+    if st.button("📊 Dashboard Overview"):
+        st.switch_page("pages/Dashboard_Overview.py")
 
-<a href="/Dashboard_Overview" 
-title="View overall analytics and visual insights of crop production data"
-style="text-decoration:none; color:black; font-weight:bold;">
-📊 Dashboard Overview
-</a>
-
-<br><br>
-
-<a href="/Climate_Impact" 
-title="Analyze how rainfall, temperature and pesticides affect crop yield"
-style="text-decoration:none; color:black; font-weight:bold;">
-🌦 Climate Impact
-</a>
-
-</div>
-""", unsafe_allow_html=True)
+with col3:
+    if st.button("🌦 Climate Impact"):
+        st.switch_page("pages/Climate_Impact.py")
