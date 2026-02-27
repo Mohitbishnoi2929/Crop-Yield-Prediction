906c0e4d9e2df7494c74e88aa1c5d60d3c1c645f
Mohitbishnoi2929 <bishnoimohit1999@gmail.com>
2026-02-27 19:23:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+# load models
+Rs = pickle.load(open('Rs.pkl','rb'))
+preprocessor = pickle.load(open('preprocessor.pkl','rb'))
+
+st.title("Crop Yield Prediction")
+
+Area = st.text_input("Area")
+Item = st.text_input("Item")
+Year = st.number_input("Year")
+Rainfall = st.number_input("Average Rainfall")
+Temp = st.number_input("Average Temperature")
+Pesticide = st.number_input("Pesticide in Tonnes")
+
+if st.button("Predict"):
+    features = np.array([[Area, Item, Year, Rainfall, Temp, Pesticide]], dtype=object)
+    transformed = preprocessor.transform(features)
+    prediction = Rs.predict(transformed)
+
+    st.success(f"Predicted Yield: {prediction[0]}")
 
