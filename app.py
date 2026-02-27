ace95419b51e904825d1748859c491f9d8d8c4f0
Mohitbishnoi2929 <bishnoimohit1999@gmail.com>
2026-02-27 21:48:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,19 +3,18 @@ import streamlit as st
 st.set_page_config(page_title="Crop Yield Dashboard", layout="wide")
 
 st.title("🌾 Crop Yield Analytics Platform")
-
 st.markdown("## 🚀 Navigate Through the Platform")
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
     if st.button("🔮 Prediction"):
-        st.switch_page("Predictive page")
+        st.switch_page("Predictive_page.py")
 
 with col2:
     if st.button("📊 Dashboard Overview"):
-        st.switch_page("Dashboard Overview")
+        st.switch_page("Dashboard_Overview.py")
 
 with col3:
     if st.button("🌦 Climate Impact"):
-        st.switch_page("Climate Impact")
+        st.switch_page("Climate_Impact.py")
