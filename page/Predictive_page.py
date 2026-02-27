2d65f14db13267a5584bc7f5da6eb03c6470cbc8
Mohitbishnoi2929 <bishnoimohit1999@gmail.com>
2026-02-27 19:31:52 +0530

    Update Predictive_page.py

--- page/Predictive_page.py
+++ page/Predictive_page.py
@@ -1,25 +1,21 @@
-from flask import Blueprint, request, render_template
+import streamlit as st
 import numpy as np
 import pickle
 
-# Create Blueprint
-predict_bp = Blueprint('predict', __name__)
-
-# Load models
 Rs = pickle.load(open('Rs.pkl','rb'))
 preprocessor = pickle.load(open('preprocessor.pkl','rb'))
 
-@predict_bp.route('/predict', methods=['POST'])
-def predict():
-    Area = request.form['Area']
-    Item = request.form['Item']
-    Year = request.form['Year']
-    Rainfall = request.form['average_rain_fall_mm_per_year']
-    Temp = request.form['avg_temp']
-    Pesticide = request.form['Pesticide_in_tonnes']
+st.title("Crop Yield Prediction")
 
+Area = st.text_input("Area")
+Item = st.text_input("Item")
+Year = st.number_input("Year")
+Rainfall = st.number_input("Rainfall")
+Temp = st.number_input("Temperature")
+Pesticide = st.number_input("Pesticide")
+
+if st.button("Predict"):
     features = np.array([[Area, Item, Year, Rainfall, Temp, Pesticide]], dtype=object)
     transformed = preprocessor.transform(features)
     prediction = Rs.predict(transformed)
-
-    return render_template('index.html', predicted_value=prediction[0])
+    st.success(f"Predicted Yield: {prediction[0]}")
